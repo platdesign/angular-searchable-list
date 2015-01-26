644de6acebfcf17484a95d6bb029850b6e3fab74
Christian Blaschke <mail@platdesign.de>
2015-01-26 21:46:41 +0100

    build and new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,69 +2,43 @@
 
 var gulp = require('gulp');
 var fs = require('fs');
-var pdGulpJsTask = require('pd-gulp-js-task');
 
+var banner = fs.readFileSync('banner.txt', 'utf-8');
 
+// Register sass tasks
+var sass = require('pd-gulp-sass-task')(gulp);
 
-
-var jsSrc = './src/angular-searchable-list.js';
-var dest = './dist';
-var standaloneName = 'pdSearchableList';
-
-
-
-gulp.task('js-dev', pdGulpJsTask({
-	lib: {
-		src: jsSrc,
-		dest: __dirname+'/dist',
-		browserify: {
-			debug:true,
-			standalone: standaloneName
-		}
-	}
-}));
-
-gulp.task('js-watch', pdGulpJsTask({
-	lib: {
-		src: jsSrc,
-		dest: dest,
-		watch: true,
-		browserify: {
-			debug:true,
-			standalone: standaloneName
-		}
-	}
-}));
-
-gulp.task('js-build', pdGulpJsTask({
-	options: {
-		banner: fs.readFileSync('./banner.txt', 'utf-8')
+sass.register({
+	options:{
+		banner: banner
 	},
-	lib: {
-		src: jsSrc,
-		dest: dest,
-		uglify: true,
+	assets: {
+		src: './src/*.scss',
+		dest: './dist'
+	}
+},{
+	watch: {
+		watch: './src/**/*.scss'
+	}
+});
+
+
+
+// Register javascript tasks
+var js = require('pd-gulp-js-task')(gulp);
+
+js.register({
+	options:{
+		banner: banner
+	},
+	assets: {
+		src: './src/*.js',
+		dest: './dist',
 		browserify: {
-			standalone: standaloneName
+			standalone: 'pdSearchableList'
 		}
 	}
-}));
+});
 
 
-
-
-gulp.task('scss-dev', function() {});
-gulp.task('scss-build', function() {});
-gulp.task('scss-watch', function() {});
-
-
-
-
-
-
-
-
-gulp.task('watch', ['js-watch', 'scss-build']);
-gulp.task('dev', ['js-dev', 'scss-dev']);
-gulp.task('build', ['js-build', 'scss-build']);
 
